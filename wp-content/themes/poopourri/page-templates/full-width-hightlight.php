bdf29a99f3d5ce1678f9c3e429a28ec6e22b804b
nealsharmon <nealsharmon@gmail.com>
2013-08-20 14:42:32 -0600

    Adjusting the highlight
    to be 540px

--- wp-content/themes/poopourri/page-templates/full-width-hightlight.php
+++ wp-content/themes/poopourri/page-templates/full-width-hightlight.php
@@ -20,19 +20,19 @@ get_header(); ?>
     padding-top: 35px;position:relative;
 ">
 			<p><img class="alignleft" alt="Spritz the bowl before-you-go &amp; no one else will ever know" src="/wp-content/themes/poopourri/images/spritz_the_bowl_header.png" width="916" height="88"></p>
-  <iframe title="YouTube video player" class="youtube-player" type="text/html" width="502" height="340" src="http://www.youtube.com/embed/bvcLWKMa_Ls?rel=0&amp;hd=1&amp;wmode=transparent&amp;autoplay=0&amp;showinfo=0&amp;color=white&amp;autohide=1&amp;rel=0&amp;modestbranding=1&amp;origin=https%3A%2F%2Forapup.com" frameborder="0" style="
+  <iframe title="YouTube video player" class="youtube-player" type="text/html" width="502" height="355" src="http://www.youtube.com/embed/bvcLWKMa_Ls?rel=0&amp;hd=1&amp;wmode=transparent&amp;autoplay=0&amp;showinfo=0&amp;color=white&amp;autohide=1&amp;rel=0&amp;modestbranding=1&amp;origin=https%3A%2F%2Forapup.com" frameborder="0" style="
     position: absolute;
     left: 0px;
-    top: 175px;
+    top: 155px;
 "></iframe>
   
-  <img src="/wp-content/themes/poopourri/images/leave_toilet_smelling_better.png" style="position:absolute;right:25px;top: 175px;">
-  <p style="position:absolute;right:28px;top:235px;width:380px;font-size:14pt">Poo~Pourri is the award-winning before-you-go toilet spray.</p><img src="/wp-content/themes/poopourri/images/good_housekeeping_seal.png" style="position:absolute;top: 285px;right:110px;">
-  <p style="position:absolute;right:28px;top: 420px;width:380px;text-align:center;font-size:14pt;font-weight:bold;color:red;">Over 4 million sold!</p>
+  <img src="/wp-content/themes/poopourri/images/leave_toilet_smelling_better.png" style="position:absolute;right:25px;top: 155px;">
+  <p style="position:absolute;right:28px;top: 220px;width:380px;font-size:14pt">Poo~Pourri is the award-winning before-you-go toilet spray.</p><img src="/wp-content/themes/poopourri/images/good_housekeeping_seal.png" style="position:absolute;top: 270px;right:110px;">
+  <p style="position:absolute;right:28px;top: 410px;width:380px;text-align:center;font-size:14pt;font-weight:bold;color:red;">Over 4 million sold!</p>
   <a href="#" class="order_now_btn" style="
     position: absolute;
     right: 30px;
-    top: 450px;
+    top: 440px;
 "></a>
   
 		</div>
