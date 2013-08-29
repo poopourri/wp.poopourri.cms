e3cf22c82441ccd83665730b619627d1a763a878
nealsharmon <nealsharmon@gmail.com>
2013-08-28 22:22:08 -0600

    header-checkout

--- wp-content/themes/poopourri/header-checkout.php
+++ wp-content/themes/poopourri/header-checkout.php
@@ -32,7 +32,7 @@
 
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
-		<header id="masthead" class="site_header" role="banner">
+		<header id="masthead" class="site_header is-minimized site_header_checkout" role="banner">
 		    <div id="header-container">
 		        <div id="header-links">
     		        <a class="shop-link shop_header_btn header-content-link" data-content="shop-content"><span class="screen-reader-text">Shop over 20 scents</span></a>
@@ -42,27 +42,6 @@
     			    <a class="follow-link follow_header_btn header-content-link" data-content="follow-content"><span class="screen-reader-text">Follow:</span><span class="following"><span class="counter">1,230,000</span> Followers</span></a>
     			</div>
 		    </div>
-		    <div id="header-content">
-                <div id="shop-content" class="header-content clearfix">
-                    <?php wp_nav_menu( array( 'theme_location' => 'shop', 'menu_class' => 'shop-menu' ) ); ?>
-                </div>
-                <div id="follow-content" class="header-content clearfix">
-                    <div class="temp-rip-me-out" style="width: 500px; margin: 0 auto; padding: 30px 0; color: #fff;">Hamburger chuck bacon, tenderloin beef salami tail ball tip venison pork belly brisket swine. Leberkas beef ribs turducken tail tongue kielbasa. Fatback t-bone corned beef chicken bresaola. Jerky biltong sausage fatback flank leberkas swine capicola meatball pork loin bresaola shank.</div>
-                </div>
-            </div>
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php if ( is_user_logged_in() ) { ?>
-						<ul id="menu-account-menu" class="nav-menu"><li id="menu-item-account" class="menu-item menu-item-type-custom menu-item-object-custom"><a href="#">My Account</a></li></ul>
-					<?php } else { ?>
-						<ul id="menu-account-menu" class="nav-menu"><li id="menu-item-account" class="menu-item menu-item-type-custom menu-item-object-custom"><a href="#">Sign In / Create Account</a></li></ul>
-					<?php } ?>
-					<?php wp_nav_menu( array( 'theme_location' => 'customer', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
             <div id="cart-container">
                 <div id="cart-content">
                     <div style="margin-top: 150px; display: block; color: #ddd; text-transform: uppercase; font-size: 20px; text-align: center;">Your Cart Is Empty</div>
@@ -72,4 +51,4 @@
 
 		</header><!-- #masthead -->
 
-		<div id="main" class="site-main">
+		<div id="main-checkout" class="site-main">
